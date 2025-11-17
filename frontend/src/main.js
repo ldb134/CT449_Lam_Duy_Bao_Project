a0358d9269f87877b5fe8c1709d2f19748c09331
Lam Duy Bao <baob2205854@student.ctu.edu.vn>
2025-11-17 15:42:38 +0700

    khoi tao frontend

---
+++ frontend/src/main.js
@@ -0,0 +1,23 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fas)
+
+const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
