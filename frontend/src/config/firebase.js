06bd51dd50cd2b7342ef3d3c5acc1d9f542d9f18
Lam Duy Bao <baob2205854@student.ctu.edu.vn>
2025-12-03 20:41:06 +0700

    loi bao mat

--- frontend/src/config/firebase.js
+++ frontend/src/config/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAjWsnMBPBCNcJMkkQicOWphgFN7D_8sI4",
-  authDomain: "library-mevn-e76ff.firebaseapp.com",
-  projectId: "library-mevn-e76ff",
-  storageBucket: "library-mevn-e76ff.firebasestorage.app",
-  messagingSenderId: "851778327192",
-  appId: "1:851778327192:web:1bcb134368859c4ca5b6ed",
-  measurementId: "G-T4E6JYKQR3"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
